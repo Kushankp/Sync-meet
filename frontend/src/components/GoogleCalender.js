7a4f24f7cf9642393627b6f42a3f6c452908711b
Pulipati Kushank <kushankrockz@gmail.com>
2024-09-22 00:52:25 -0500

    Authorize Button

--- frontend/src/components/GoogleCalender.js
+++ frontend/src/components/GoogleCalender.js
@@ -77,6 +77,7 @@ function GoogleCalendar() {
   return (
     <div>
       <p>Google Calendar API</p>
+      <button onClick={handleAuthClick}>Authorize</button>
       <pre>{events.length ? `Events:\n${events.join('\n')}` : 'No events found'}</pre>
     </div>
   );
